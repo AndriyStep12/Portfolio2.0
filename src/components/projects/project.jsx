27b0129163fff97e436feb58349425a1618bd79f
Andriy_Step <andystep2008@gmail.com>
2025-01-16 19:37:32 +0200

    update

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,52 +1,102 @@
+// import './project.css';
+// import Card from './Card/Card';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// function ProjectsPage({isActive, GITHUB_TOKEN}) {
+//     const [repos, setRepos] = useState([]);
+
+
+//     async function fetchAllRepos() {
+//         let page = 1;
+//         let allRepos = [];
+//         let hasNextPage = true;
+    
+//         while (hasNextPage) {
+//             const response = await axios.get("https://api.github.com/user/repos", {
+//                 headers: {
+//                     Authorization: `token ${GITHUB_TOKEN}`
+//                 },
+//                 params: {
+//                     visibility: 'all',
+//                     per_page: 100,
+//                     page: page
+//                 }
+//             });
+//             allRepos = allRepos.concat(response.data);
+//             hasNextPage = response.headers.link && response.headers.link.includes('rel="next"');
+//             page++;
+//         }
+    
+//         return allRepos;
+//     }
+    
+//     useEffect(() => {
+//         async function fetchData() {
+//             try {
+//                 const data = await fetchAllRepos();
+//                 const filteredRepos = data.filter(repo => repo.description && repo.homepage && repo.name !== 'Portfolio2.0' && repo.name !== 'Portfolio' && repo.name !== 'meeting-platform-server');
+//                 filteredRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+//                 setRepos(filteredRepos);
+//             } catch (error) {
+//                 console.error(error);
+//                 setRepos([]);
+//             }
+//         }
+    
+//         fetchData();
+//     }, []);
+    
+
+
+
+//     return (
+//         <div className="projectCards" style={!isActive ? {marginLeft: '0%', overflowY: 'hidden', transition: '1s', display: 'flex'}: {display: 'none', marginLeft: '-100%', overflow: 'hidden', transition: '1s'}}>
+//             <h2>My Projects</h2>
+//             <div className="projects-cards">
+//                 {repos.map(item => (
+//                     <Card
+//                         key={item.id}
+//                         homepage={item.homepage}
+//                         name={item.name}
+//                         likes={item.stargazers_count}
+//                         link={item.html_url}
+//                         description={item.description}
+//                     />
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default ProjectsPage;
+
 import './project.css';
 import Card from './Card/Card';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function ProjectsPage({isActive, GITHUB_TOKEN}) {
+function ProjectsPage({isActive}) {
     const [repos, setRepos] = useState([]);
 
-
-    async function fetchAllRepos() {
-        let page = 1;
-        let allRepos = [];
-        let hasNextPage = true;
-    
-        while (hasNextPage) {
-            const response = await axios.get("https://api.github.com/user/repos", {
-                headers: {
-                    Authorization: `token ${GITHUB_TOKEN}`
-                },
-                params: {
-                    visibility: 'all',
-                    per_page: 100,
-                    page: page
-                }
+useEffect(() => {
+    async function fetchData() {
+        try {
+            const response = await axios.get("https://api.github.com/users/AndriyStep12/repos?page=1&per_page=100");
+            const data = response.data;
+            const filteredRepos = data.filter(repo => repo.description != null & repo.homepage != '' & repo.homepage != null &  repo.name != 'Portfolio2.0');
+            filteredRepos.sort(function(a, b) {
+                return a.stargazers_count - b.stargazers_count;
             });
-            allRepos = allRepos.concat(response.data);
-            hasNextPage = response.headers.link && response.headers.link.includes('rel="next"');
-            page++;
+            filteredRepos.reverse()
+            setRepos(filteredRepos);
+        } catch (error) {
+            console.error(error);
+            setRepos([]);
         }
-    
-        return allRepos;
     }
-    
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const data = await fetchAllRepos();
-                const filteredRepos = data.filter(repo => repo.description && repo.homepage && repo.name !== 'Portfolio2.0' && repo.name !== 'Portfolio' && repo.name !== 'meeting-platform-server');
-                filteredRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
-                setRepos(filteredRepos);
-            } catch (error) {
-                console.error(error);
-                setRepos([]);
-            }
-        }
-    
-        fetchData();
-    }, []);
-    
+    fetchData();
+}, []);
 
 
 
